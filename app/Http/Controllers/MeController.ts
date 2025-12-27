aa936ffd35c3600cc0fd18fa642cfcd0f9acf620
Throy Towercamp <166675577+FDC-Genesis@users.noreply.github.com>
2025-12-27 16:09:48 +0800

    fix

--- app/Http/Controllers/MeController.ts
+++ app/Http/Controllers/MeController.ts
@@ -9,7 +9,21 @@ class MeController extends Controller {
     // create function like this
     public index: HttpDispatch = async () => {
         // your logic here
-        const projects = await Project.all();
+        const projects = [
+            {
+                project_name: "Honovel",
+                description:
+                "A Laravel-like typescript-only web framework powered by Deno and Hono.",
+                github_url: "https://github.com/kiratrizon/honovel",
+                live_demo_url: "https://honovel.deno.dev",
+            },
+            {
+                project_name: "Sentiment Analysis (Unoptimized)",
+                description: "A web application that analyzes the sentiment of a given text using machine learning.",
+                // github_url: "https://github.com/kiratrizon/nemsu-sentiment-analysis",
+                live_demo_url: "https://sentiment-analysis.kiratrizon.deno.net",
+            }
+        ];
         const name = "Genesis Troy Torrecampo";
         const skills = {
             languages: ["HTML", "CSS", "JavaScript", "TypeScript", "PHP"],
