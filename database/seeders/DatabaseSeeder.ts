349113c9cd522dd4e52c29c584b1a81b2a126fc9
kyuuukira <genesistroy.fdc@gmail.com>
2026-07-31 15:37:53 +0800

    Update From Honovel

--- database/seeders/DatabaseSeeder.ts
+++ database/seeders/DatabaseSeeder.ts
@@ -3,21 +3,29 @@ import Project from "App/Models/Project.ts";
 
 export default class DatabaseSeeder extends Seeder {
   public async run() {
-
     const projects = [
       {
         project_name: "Honovel",
         description:
           "A Laravel-like typescript-only web framework powered by Deno and Hono.",
         github_url: "https://github.com/kiratrizon/honovel",
-        live_demo_url: "https://honovel.deno.dev",
+        live_demo_url: "https://honovel.kiratrizon.deno.net",
       },
       {
         project_name: "Sentiment Analysis (Unoptimized)",
-        description: "A web application that analyzes the sentiment of a given text using machine learning.",
+        description:
+          "A web application that analyzes the sentiment of a given text using machine learning.",
         github_url: "https://github.com/kiratrizon/nemsu-sentiment-analysis",
         live_demo_url: "https://sentiment-analysis.kiratrizon.deno.net",
-      }
+      },
+      {
+        project_name:
+          "PeculiarLads website w/ discord bot. (Currently running on local tunnel)",
+        description:
+          "Just a member monitoring in entire guild for the game called Dragon Nest. A first project using Honovel.",
+        github_url: "https://github.com/kiratrizon/peculiar-lads",
+        live_demo_url: "https://peculiarlads.kiratrizon.deno.net",
+      },
     ];
 
     for (const project of projects) {
