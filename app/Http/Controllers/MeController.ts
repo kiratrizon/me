e4bc4e58979a781b98c30d33db0085a60677c620
kyuuukira <genesistroy.fdc@gmail.com>
2026-07-31 15:46:32 +0800

    update resume

--- app/Http/Controllers/MeController.ts
+++ app/Http/Controllers/MeController.ts
@@ -1,184 +1,206 @@
 import Controller from "App/Http/Controllers/Controller.ts";
-import {RtcTokenBuilder, RtcRole} from "agora-token";
+import { RtcTokenBuilder, RtcRole } from "agora-token";
 
 // make manual mailer
 
 import { Cache } from "Illuminate/Support/Facades/index.ts";
 import Channel from "../../Models/Channel.ts";
 
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 class MeController extends Controller {
+  private static resendClient: Resend;
+  // create function like this
+  public index: HttpDispatch = async () => {
+    // your logic here
+    const projects = [
+      {
+        project_name: "Honovel",
+        description:
+          "A Laravel-like typescript-only web framework powered by Deno and Hono.",
+        github_url: "https://github.com/kiratrizon/honovel",
+        live_demo_url: "https://honovel.kiratrizon.deno.net",
+      },
+      {
+        project_name: "Sentiment Analysis (Unoptimized)",
+        description:
+          "A web application that analyzes the sentiment of a given text using machine learning.",
+        github_url: "https://github.com/kiratrizon/nemsu-sentiment-analysis",
+        // live_demo_url: "https://sentiment-analysis.kiratrizon.deno.net",
+      },
+      {
+        project_name:
+          "PeculiarLads website w/ discord bot. (Currently running on local tunnel)",
+        description:
+          "Just a member monitoring in entire guild for the game called Dragon Nest. A first project using Honovel.",
+        github_url: "https://github.com/kiratrizon/peculiar-lads",
+        live_demo_url: "https://peculiarlads.kiratrizon.deno.net",
+      },
+    ];
+    const name = "Genesis Troy Torrecampo";
+    const skills = {
+      languages: ["HTML", "CSS", "JavaScript", "TypeScript", "PHP"],
+      frameworks: ["Express.js", "Hono.js", "Laravel", "CakePHP", "React.js"],
+      databases: ["MySQL", "MongoDB", "DynamoDB"],
+      tools: ["Git", "Docker", "Postman", "VS Code"],
+      runtime: ["Deno", "Node.js"],
+      "Third-party Services": ["AWS", "Clever Cloud", "Google Console"],
+      deployment: ["Vercel", "Deno Deploy"],
+    };
 
-    private static resendClient: Resend;
-    // create function like this
-    public index: HttpDispatch = async () => {
-        // your logic here
-        const projects = [
-            {
-                project_name: "Honovel",
-                description:
-                "A Laravel-like typescript-only web framework powered by Deno and Hono.",
-                github_url: "https://github.com/kiratrizon/honovel",
-                live_demo_url: "https://honovel.deno.dev",
-            },
-            {
-                project_name: "Sentiment Analysis (Unoptimized)",
-                description: "A web application that analyzes the sentiment of a given text using machine learning.",
-                github_url: "https://github.com/kiratrizon/nemsu-sentiment-analysis",
-                // live_demo_url: "https://sentiment-analysis.kiratrizon.deno.net",
-            }
-        ];
-        const name = "Genesis Troy Torrecampo";
-        const skills = {
-            languages: ["HTML", "CSS", "JavaScript", "TypeScript", "PHP"],
-            frameworks: ["Express.js", "Hono.js", "Laravel", "CakePHP", "React.js"],
-            databases: ["MySQL", "MongoDB", "DynamoDB"],
-            tools: ["Git", "Docker", "Postman", "VS Code"],
-            runtime: ["Deno", "Node.js"],
-            "Third-party Services": ["AWS", "Clever Cloud", "Google Console"],
-            deployment: ["Vercel", "Deno Deploy"],
-        };
+    return view("me", { projects, name, skills });
+  };
 
-        return view("me", { projects, name, skills });
+  public sendMail: HttpDispatch = async ({ request }) => {
+    const body = await request.validate({
+      name: "required",
+      email: "required|email",
+      message: "required|min:10",
+    });
+
+    MeController.initResendClient();
+    const now = date("Y-m-d");
+    try {
+      const cacheLimit = ((await Cache.get(`resend_limit`)) || {
+        date: now,
+        count: 0,
+      }) as { date: string; count: number };
+      if (cacheLimit) {
+        if (cacheLimit.date === now) {
+          cacheLimit.count++;
+        } else {
+          cacheLimit.date = now;
+          cacheLimit.count = 1;
+        }
+      }
+      // save
+      await Cache.put(`resend_limit`, cacheLimit, 86400);
+      if (cacheLimit.count < 101) {
+        await MeController.resendClient.emails.send({
+          from: `${body.name} <onboarding@resend.dev>`,
+          to: "tgenesistroy@gmail.com",
+          subject: "Employer Message",
+          html: `<p>You have a new message from (${body.email}):</p>
+                        <p>${body.message}</p>`,
+        });
+      } else {
+        throw new Error("Resend limit exceeded");
+      }
+    } catch (error) {
+      console.log({
+        from: `${body.name} <onboarding@resend.dev>`,
+        to: "tgenesistroy@gmail.com",
+        subject: "Employer Message",
+        html: `<p>You have a new message from (${body.email}):</p>
+                    <p>${body.message}</p>`,
+      });
+      console.error("Error sending email:", error);
+      return response().json(
+        { success: false, error: "Failed to send email." },
+        500,
+      );
     }
 
-    public sendMail: HttpDispatch = async ({request}) => {
-        const body = await request.validate({
-            name: "required",
-            email: "required|email",
-            message: "required|min:10",
-        })
+    return response().json({ success: true });
+  };
 
-        MeController.initResendClient();
-        const now = date("Y-m-d");
-        try {
-            const cacheLimit = (await Cache.get(`resend_limit`) || {date: now, count: 0}) as {date:string, count:number};
-            if (cacheLimit) {
-                if (cacheLimit.date === now) {
-                    cacheLimit.count++;
-                } else {
-                    cacheLimit.date = now;
-                    cacheLimit.count = 1;
-                }
-            }
-            // save
-            await Cache.put(`resend_limit`, cacheLimit, 86400);
-            if (cacheLimit.count < 101){
-                await MeController.resendClient.emails.send({
-                    from: `${body.name} <onboarding@resend.dev>`,
-                    to: "tgenesistroy@gmail.com",
-                    subject: "Employer Message",
-                    html: `<p>You have a new message from (${body.email}):</p>
-                        <p>${body.message}</p>`
-                });
-            } else {
-                throw new Error("Resend limit exceeded");
-            }
-        } catch (error) {
-            console.log({
-                from: `${body.name} <onboarding@resend.dev>`,
-                to: "tgenesistroy@gmail.com",
-                subject: "Employer Message",
-                html: `<p>You have a new message from (${body.email}):</p>
-                    <p>${body.message}</p>`
-            })
-            console.error("Error sending email:", error);
-            return response().json({ success: false, error: "Failed to send email." }, 500);
-        }
+  public vc: HttpDispatch = async ({ request }) => {
+    const appId = env("AGORA_APP_ID") as string;
+    const appCert = env("AGORA_APP_CERT") as string;
+    const sessId = request.session.getId();
+    // generate random uid
+    const uid = 0;
 
-        return response().json({ success: true });
+    const role = RtcRole.PUBLISHER;
+    const tokenExpirationInSecond = 3600;
+    const privilegeExpirationInSecond = 3600;
+    // generate random channel name
+    let channelName = (await Cache.get(`agora_channel_${sessId}`)) as string;
+
+    const isSaved = !!channelName;
+    if (!isSaved) {
+      channelName = `channel-${uid}-${Date.now()}`;
+    }
+    if (!isset(appCert)) {
+      abort(400, "Missing Agora credentials");
     }
 
-    public vc: HttpDispatch = async ({request}) => {
-        const appId = env("AGORA_APP_ID") as string;
-        const appCert = env("AGORA_APP_CERT") as string;
-        const sessId = request.session.getId();
-        // generate random uid
-        const uid = 0;
+    const tokenWithUid =
+      (await Cache.get(`agora_token_${sessId}`)) ||
+      RtcTokenBuilder.buildTokenWithUid(
+        appId,
+        appCert,
+        channelName,
+        uid,
+        role,
+        tokenExpirationInSecond,
+        privilegeExpirationInSecond,
+      );
 
-        const role = RtcRole.PUBLISHER;
-        const tokenExpirationInSecond = 3600
-        const privilegeExpirationInSecond = 3600
-        // generate random channel name
-        let channelName = await Cache.get(`agora_channel_${sessId}`) as string;
+    // save
+    if (!isSaved) {
+      await Cache.put(`agora_token_${sessId}`, tokenWithUid, 3600);
+      await Cache.put(`agora_channel_${sessId}`, channelName, 3600);
 
-        const isSaved = !!channelName;
-        if (!isSaved) {
-            channelName = `channel-${uid}-${Date.now()}`;
+      MeController.initResendClient();
+
+      await Channel.create({
+        channel_name: channelName,
+        token: tokenWithUid,
+      });
+    }
+    const now = date("Y-m-d");
+    const cacheLimit = ((await Cache.get(`vc_limit_${sessId}`)) || {
+      date: now,
+      count: 0,
+    }) as { date: string; count: number };
+    if (cacheLimit) {
+      if (cacheLimit.date === now) {
+        cacheLimit.count++;
+      } else {
+        cacheLimit.date = now;
+        cacheLimit.count = 1;
+      }
+    }
+    if (cacheLimit.count < 10) {
+      try {
+        if (
+          (config("app").env !== "local" && cacheLimit.count < 10) ||
+          (config("app").env === "local" && cacheLimit.count < 3)
+        ) {
+          await MeController.resendClient.emails.send({
+            from: `App <onboarding@resend.dev>`,
+            to: "tgenesistroy@gmail.com",
+            subject: "Employer Wants to Connect",
+            html: `<p>Connect here: ${config("app").url}/connectVC?channel=${channelName}</p>`,
+          });
         }
-        if (!isset(appCert)) {
-            abort(400, "Missing Agora credentials");
-        }
-        
-        const tokenWithUid = await Cache.get(`agora_token_${sessId}`) || RtcTokenBuilder.buildTokenWithUid(
-            appId,
-            appCert,
-            channelName,
-            uid,
-            role,
-            tokenExpirationInSecond,
-            privilegeExpirationInSecond
-        );
-
-        // save
-        if (!isSaved) {
-            await Cache.put(`agora_token_${sessId}`, tokenWithUid, 3600);
-            await Cache.put(`agora_channel_${sessId}`, channelName, 3600);
-
-            MeController.initResendClient();
-
-            await Channel.create({
-                channel_name: channelName,
-                token: tokenWithUid,
-            });
-        }
-        const now = date("Y-m-d");
-        const cacheLimit = (await Cache.get(`vc_limit_${sessId}`) || {date: now, count: 0}) as {date:string, count:number};
-        if (cacheLimit) {
-            if (cacheLimit.date === now) {
-                cacheLimit.count++;
-            } else {
-                cacheLimit.date = now;
-                cacheLimit.count = 1;
-            }
-        }
-        if (cacheLimit.count < 10) {
-            try {
-                if ((config("app").env !== "local" && cacheLimit.count < 10) || (config("app").env === "local" && cacheLimit.count < 3)) {
-                    await MeController.resendClient.emails.send({
-                        from: `App <onboarding@resend.dev>`,
-                        to: "tgenesistroy@gmail.com",
-                        subject: "Employer Wants to Connect",
-                        html: `<p>Connect here: ${config("app").url}/connectVC?channel=${channelName}</p>`
-                    });
-                }
-            } catch (error) {
-                console.error("Error sending email:", error);
-            }
-        }
-
-        return view("vc", { token: tokenWithUid, channelName });
+      } catch (error) {
+        console.error("Error sending email:", error);
+      }
     }
 
-    public connectVC: HttpDispatch = async ({request}) => {
-        const channelName = request.query("channel");
-        const appId = env("AGORA_APP_ID") as string;
-        const row = await Channel.where("channel_name", channelName).first();
-        const token = row?.token;
+    return view("vc", { token: tokenWithUid, channelName });
+  };
 
-        if (!token || !channelName) {
-            abort(400, "Missing token or channel");
-        }
+  public connectVC: HttpDispatch = async ({ request }) => {
+    const channelName = request.query("channel");
+    const appId = env("AGORA_APP_ID") as string;
+    const row = await Channel.where("channel_name", channelName).first();
+    const token = row?.token;
 
-        return view("vc", { token, channelName, appId });
+    if (!token || !channelName) {
+      abort(400, "Missing token or channel");
     }
 
-    private static initResendClient() {
-        if (!MeController.resendClient) {
-            MeController.resendClient = new Resend(env("RESEND_TOKEN") as string);
-        }
+    return view("vc", { token, channelName, appId });
+  };
+
+  private static initResendClient() {
+    if (!MeController.resendClient) {
+      MeController.resendClient = new Resend(env("RESEND_TOKEN") as string);
     }
+  }
 }
 
 export default MeController;
--- routes/web.ts
+++ routes/web.ts
@@ -9,13 +9,10 @@ Route.get("/vc", [MeController, "vc"]);
 Route.get("/connectVC", [MeController, "connectVC"]);
 
 Route.get("/resume", async () => {
-    let fileName = "genesis-troy-torrecampo.pdf";
-    if (config("app").env !== "local") {
-        if (config("app").url == "https://iam-throy.kiratrizon.deno.net") {
-            fileName = "genesis-troy-torrecampo-2.pdf";
-        }
-    }
-    return response().download(basePath(fileName),  "Genesis Troy Torrecampo (Web Developer).pdf");
+  return response().download(
+    basePath("genesis-troy-torrecampo.pdf"),
+    "Genesis Troy Torrecampo (Web Developer).pdf",
+  );
 });
 
 Route.post("/send-message", [MessageController, "sendMessage"]);
